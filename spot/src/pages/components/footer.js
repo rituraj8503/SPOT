e6cff0683378a6c3ef1cfea9e9702ebfb624463a
sohamsraut <sohamr@uw.edu>
2023-10-14 12:07:45 -0700

    adds color scheme

--- spot/src/pages/components/footer.js
+++ spot/src/pages/components/footer.js
@@ -1,11 +1,11 @@
 export default function footer() {
   return (
     <footer
-    className="bg-[#ffffff] w-full backdrop-opacity-10 text-[#1b1b2b]"
+    className="bg-[#0d95ff] w-full backdrop-opacity-10 text-[#f5f6f7]"
     >
       <div className="max-w-screen-3xl flex flex-col items-center justify-between">
         <div className="text-4xl mt-16 font-bold heading">Spot</div>
-        <div className="text-sm mb-20">Copuright &copy; Spot, 2023</div>
+        <div className="text-sm mb-20">Copyright &copy; Spot, 2023</div>
       </div>
     </footer>
   );
