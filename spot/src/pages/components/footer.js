125dcdb331efdff1412343dfcd00fd666aef61f0
sohamsraut <sohamr@uw.edu>
2023-10-14 12:54:29 -0700

    changes footer color

--- spot/src/pages/components/footer.js
+++ spot/src/pages/components/footer.js
@@ -4,7 +4,7 @@ export default function footer() {
     className="bg-[#0d95ff] w-full backdrop-opacity-10 text-[#f5f6f7]"
     >
       <div className="max-w-screen-3xl flex flex-col items-center justify-between">
-        <div className="text-4xl mt-16 font-bold heading">Spot</div>
+        <div className="text-4xl mt-16 font-bold heading">SPOT.</div>
         <div className="text-sm mb-20">Copyright &copy; Spot, 2023</div>
       </div>
     </footer>
